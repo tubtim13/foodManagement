deb75d07f6ab1ffbcd446a66355065813c938a49
Tubtim <tubtim.kuph@gmail.com>
2022-04-08 15:13:13 +0700

    init project

---
+++ src/test/java/com/project/foodManagement/FoodManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.foodManagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FoodManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
